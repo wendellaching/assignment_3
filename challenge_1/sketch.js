d07afac8a685c6beed55370717d8fb09244a0b66
wendellaching <vincent23>
2015-09-25 16:29:11 -0400

    saved_1

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -24,23 +24,20 @@
 //             // rect(458,201,50,50);
 
 // a simple p5 sketch that demonstrates for loops
+// a simple p5 sketch that demonstrates for loops
 
 function setup() {
 	createCanvas(640, 480);
+	stroke(255);
 }
+
 function draw() {
 	background(50, 50, 50);
-	colorMode(HSB,100)
-			for (var y = 0; y <4; y++) {
-	        stroke(0,0,100);
-					fill(0, 100, y*10);
-  		}
-	    for (var x = 0; x < 10; x++) {
-        	rect(x * 65.5, y+200 ,50, 50);
-				}
 
-	    	for (var x = 0; x < 10; x++) {
-			rect(x * 65.5, y+200 ,50, 50);
-		}
+	for (i=0; i<10; i++) {
+	    fill(i*30,0,0);
+	    rect(i * 65.5, 210 ,50, 50);
+	}
+	}
 
-}
+	
