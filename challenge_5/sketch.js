868016a34e7e84145d119b52aaf76a90ec21ab62
wendellaching <vincent23>
2015-09-25 18:20:03 -0400

    fixed the mouse
    chaser

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,6 +1,7 @@
 // a simple p5 sketch that draws a cloud thing
 // at the location of the mouse cursor
-
+var ballX=100;
+var ballY=100;
 function setup() {
 	createCanvas(640, 480);
 
@@ -9,7 +10,18 @@ function setup() {
 function draw() {
 	background(0, 0, 0);
 	noStroke();
+
 	fill(255, 255, 255);
+		 if (mouseX > ballX){
+			 	ballX += 1;
+		 } else {
+			 ballX -=1;
+		 }
+		 if (mouseY > ballY){
+			 	ballY +=1
+		 } else {
+			 ballY -= -1;
+		 }
 
 	for (var i = 0; i < 1; i++){
 		ellipse(mouseX+random(0, 10), mouseY+random(0, 10), 50, 50);
