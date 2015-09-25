91895cd4a5e32e6f95e6a2ac70193a93fcdd9d84
wendellaching <vincent23>
2015-09-24 21:50:40 -0400

    changed background and ellipse
    background to black, and ellipse to 50,50

--- challenge_4/sketch.js
+++ challenge_4/sketch.js
@@ -1,18 +1,22 @@
+var ballX = 200;
+var ballY = 100;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
+
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(640, 480);
 	noStroke();
 	noLoop();
 }
 
 function draw() {
 	// clear the background
-	background(150, 50, 50);
+	background(0, 0, 0);
 
 	// set a fill color
 	fill(255, 255, 255);
 
 	// draw the ellipse
-	var diameter = random(100, 200);
+	var diameter = random(50, 50);
 	ellipse(320, 180, diameter, diameter);
 }
